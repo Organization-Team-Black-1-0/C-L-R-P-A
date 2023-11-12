743b931cd2cf85dd7e24ff9dc68a4a236373dc4e
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-11-12 06:35:21 -0500

    Pruebas con medicion anormal

--- main.js
+++ main.js
@@ -6,22 +6,21 @@ function verifyinputFormPresion() {
   var numeroregistroPresionS = document.getElementById("registroPresionS").value;
   var numeroregistroPresionD = document.getElementById("registroPresionD").value;
   if (!Number.isNaN(numeroregistroPresionD) && !Number.isNaN(numeroregistroPresionS)) {
-
     var alertadeRegistroM1 = document.getElementById("alertadeRegistroP1");
-
-
+    /*var alertadeRegistroM2 = document.getElementById("alertadeRegistroP2");*/
     /*    alert("El número que introdujo es " + numeroregistroPresionS + " / " + numeroregistroPresionD); */
 
+  }
+
+  else { alert("El valor introducido no es un número"); };
+    
+ 
 
 
 
+  alertadeRegistroM1.innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
 
-  } else { alert("El valor introducido no es un número"); }
-
-
-  alertadeRegistroM1.innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual  + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
   return false;
-
 
 
 }
