6e348554d7ab707e3d3ea6ebbe6cac83d3a5ae85
Parasitic-art-guild <plan.mantenimiento.industrial@gmail.com>
2023-11-12 21:05:38 -0500

    Resuelto cambio de color en mensaje

--- main.js
+++ main.js
@@ -3,42 +3,38 @@
 function verifyinputFormPresion() {
 
   const fechaHoraActual = new Date();
+  const coloralerta = "#FFF200";
+  const colornormal = "#00FF99";
+
   var numeroregistroPresionS = document.getElementById("registroPresionS").value;
   var numeroregistroPresionD = document.getElementById("registroPresionD").value;
   if (!Number.isNaN(numeroregistroPresionD) && !Number.isNaN(numeroregistroPresionS)) {
     /* var alertadeRegistroM1 = document.getElementById("alertadeRegistroP1");*/
     document.getElementById("alertadeRegistroP1").innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
-
-
   }
   else {
     document.getElementById("alertadeRegistroP1").innerHTML = '<h5> El valor introducido no es numero </h5>';
-
-
-    /*alert("El valor introducido no es un número")*/
+ 
   };
-  /* alertadeRegistroM1.innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
- */
+  
   if (numeroregistroPresionS > numeroregistroPresionD) {
-    /* var alertadeRegistroM1 = document.getElementById("alertadeRegistroP1");*/
-    document.getElementById("alertadeRegistroP1").innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
 
+    document.getElementById("alertadeRegistroP1").innerHTML = '<h5> El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
+    alertadeRegistroP1.style.backgroundColor = "#00FF99";
   } else {
     document.getElementById("alertadeRegistroP1").innerHTML = '<h5> La presion siastolica debe ser mayor a la diastolica </h5>';
+    alertadeRegistroP1.style.backgroundColor = "#FFF200";
   };
 
   if (numeroregistroPresionS > 121 || numeroregistroPresionD > 81) {
     /* var alertadeRegistroM1 = document.getElementById("alertadeRegistroP1");*/
     document.getElementById("alertadeRegistroP1").innerHTML = '<h5> - LA PRESION ESTA FUERA DE LO NORMAL - AVISA A PERSONAL CERTIFICADO EN SALUD Y MEDICINA - El registro que introdujo el -- <span>' + fechaHoraActual + '</span> -- fue <span>' + numeroregistroPresionS + '</span> / <span>' + numeroregistroPresionD + '</span> </h5>';
-    
+    alertadeRegistroP1.style.background = "#FFF200";
   };
-
-
   return false;
 
 
 }
-
 
 document.querySelector("button").addEventListener("click", verifyinputFormPresion);
 
